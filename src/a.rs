4e6b90e923012e87edc4c0adc5029fbb67d585ea
Kelly Norton <kellegous@gmail.com>
2022-11-27 14:24:59 -0500

    tinkering

--- src/a.rs
+++ src/a.rs
@@ -1,8 +1,7 @@
 use cairo::Context;
 use rand::{Rng, RngCore};
-use sketches::{Color, RenderOpts};
+use sketches::{Color, RenderOpts, Size};
 use std::error::Error;
-use std::f64::consts::PI;
 
 fn select_colors(rng: &mut dyn RngCore, colors: &[Color]) -> (Color, Vec<Color>) {
     let iter = colors
@@ -26,50 +25,64 @@ fn select_colors(rng: &mut dyn RngCore, colors: &[Color]) -> (Color, Vec<Color>)
     (bg, colors)
 }
 
-fn create_series(
-    rng: &mut dyn RngCore,
-    width: f64,
-    height: f64,
+struct Grid {
     nw: i32,
     nh: i32,
-) -> (Vec<(f64, f64)>, Vec<(f64, f64)>) {
-    let dw = width / nw as f64;
-    let dh = height / nh as f64;
+}
+
+impl Grid {
+    fn new(nw: i32, nh: i32) -> Grid {
+        Grid { nw, nh }
+    }
+
+    fn gen_series<'a>(&'a self, rng: &'a mut dyn RngCore) -> impl Iterator<Item = (i32, i32)> + '_ {
+        (0..self.nw).map(|i| (i, rng.gen_range(0..self.nh)))
+    }
+
+    fn nw(&self) -> i32 {
+        self.nw
+    }
+
+    fn nh(&self) -> i32 {
+        self.nh
+    }
+}
+
+fn render_series(
+    ctx: &Context,
+    size: &Size,
+    grid: &Grid,
+    series: &[(i32, i32)],
+    color: Color,
+) -> Result<(), Box<dyn Error>> {
+    let width = size.width() as f64;
+    let height = size.height() as f64;
+
+    let dw = width / grid.nw() as f64;
+    let dh = height / grid.nh() as f64;
 
     let ow = dw / 2.0;
     let oh = dh / 2.0;
 
-    let mut sa = Vec::with_capacity(2 + nw as usize);
-    let mut sb = Vec::with_capacity(2 + nw as usize);
-
-    let ya = rng.gen_range(0..nh);
-    let yb = rng.gen_range(ya..nh);
-    let ya = oh + dh * ya as f64;
-    let yb = oh + dh * yb as f64;
-
-    sa.push((0.0, ya));
-    sa.push((ow, ya));
-    sb.push((0.0, yb));
-    sb.push((ow, yb));
-
-    for i in 1..nw - 1 {
-        let x = ow + dw * i as f64;
-        let ya = rng.gen_range(0..nh);
-        sa.push((x, oh + dh * ya as f64));
-        let yb = rng.gen_range(ya..nh);
-        sb.push((x, oh + dh * yb as f64));
+    ctx.save()?;
+    ctx.new_path();
+    ctx.move_to(0.0, 0.0);
+    let (_, y) = series.first().unwrap();
+    ctx.line_to(0.0, oh + dh * *y as f64);
+    for (x, y) in series {
+        ctx.line_to(ow + dw * *x as f64, oh + dh * *y as f64);
     }
 
-    let ya = rng.gen_range(0..nh);
-    let yb = rng.gen_range(ya..nh);
-    let ya = oh + dh * ya as f64;
-    let yb = oh + dh * yb as f64;
-    sa.push((width - ow, ya));
-    sa.push((width, ya));
-    sb.push((width - ow, yb));
-    sb.push((width, yb));
+    let (_, y) = series.last().unwrap();
+    ctx.line_to(width, oh + dh * *y as f64);
+    ctx.line_to(width, 0.0);
+    ctx.close_path();
+    color.set_with_alpha(ctx, 0.6);
+    ctx.fill()?;
 
-    (sa, sb)
+    ctx.restore()?;
+
+    Ok(())
 }
 
 fn render_grid(
@@ -77,10 +90,12 @@ fn render_grid(
     color: Color,
     width: f64,
     height: f64,
-    nw: i32,
-    nh: i32,
+    grid: &Grid,
 ) -> Result<(), Box<dyn Error>> {
     color.set_with_alpha(ctx, 0.6);
+
+    let nw = grid.nw();
+    let nh = grid.nh();
 
     let dw = width / nw as f64;
     let dh = height / nh as f64;
@@ -114,35 +129,12 @@ fn render_grid(
     Ok(())
 }
 
-fn render_points(
-    ctx: &Context,
-    series: &[(f64, f64)],
-    bg: Color,
-    fg: Color,
-) -> Result<(), Box<dyn Error>> {
-    ctx.save()?;
-    ctx.set_line_width(1.0);
-    for (x, y) in series.iter() {
-        ctx.new_path();
-        ctx.arc(*x, *y, 3.0, 0.0, 2.0 * PI);
-        bg.set(ctx);
-        ctx.fill_preserve()?;
-        fg.set(ctx);
-        ctx.stroke()?;
-    }
-    ctx.restore()?;
-    Ok(())
-}
-
 pub fn render(opts: &dyn RenderOpts, ctx: &Context) -> Result<(), Box<dyn Error>> {
     let size = opts.size();
     let width = size.width() as f64;
     let height = size.height() as f64;
 
     let mut rng = opts.rng();
-
-    let nw = rng.gen_range(5..40);
-    let nh = rng.gen_range(5..40);
 
     let themes = opts.themes()?;
     let (_, theme) = themes.pick(&mut rng);
@@ -152,6 +144,7 @@ pub fn render(opts: &dyn RenderOpts, ctx: &Context) -> Result<(), Box<dyn Error>
     ctx.rectangle(0.0, 0.0, width, height);
     ctx.fill()?;
 
+    let grid = Grid::new(rng.gen_range(5..40), rng.gen_range(5..40));
     render_grid(
         ctx,
         if bg.luminance() > 0.5 {
@@ -161,40 +154,11 @@ pub fn render(opts: &dyn RenderOpts, ctx: &Context) -> Result<(), Box<dyn Error>
         },
         width,
         height,
-        nw,
-        nh,
+        &grid,
     )?;
 
-    let (sa, sb) = create_series(&mut rng, width, height, nw, nh);
-
-    ctx.save()?;
-    ctx.new_path();
-    ctx.move_to(0.0, 0.0);
-    for (x, y) in sa.iter() {
-        ctx.line_to(*x, *y);
-    }
-    ctx.line_to(width, 0.0);
-    ctx.close_path();
-    theme[0].set_with_alpha(ctx, 0.6);
-    ctx.fill()?;
-    ctx.restore()?;
-
-    ctx.save()?;
-    ctx.new_path();
-    ctx.move_to(0.0, height);
-    for (x, y) in sb.iter() {
-        ctx.line_to(*x, *y);
-    }
-    ctx.line_to(width, height);
-    ctx.close_path();
-    theme[1].set_with_alpha(ctx, 0.6);
-    ctx.fill()?;
-    ctx.restore()?;
-
-    let fg = Color::from_u32(0x000000);
-    let bg = Color::from_u32(0xffffff);
-    render_points(ctx, &sa, bg, fg)?;
-    render_points(ctx, &sb, bg, fg)?;
+    let series = grid.gen_series(&mut rng).collect::<Vec<_>>();
+    render_series(ctx, &size, &grid, &series, theme[0])?;
 
     Ok(())
 }
