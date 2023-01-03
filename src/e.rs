afb3143adacf844b2a214cc34b9ab8754e5ef6e6
Kelly Norton <kellegous@gmail.com>
2023-01-02 21:57:20 -0500

    .

--- src/e.rs
+++ src/e.rs
@@ -19,6 +19,42 @@ fn color_contrasting_with(c: &Color) -> Color {
     }
 }
 
+fn index_of_max(colors: &[Color]) -> usize {
+    colors
+        .iter()
+        .map(|c| c.luminance())
+        .enumerate()
+        .max_by(|(_, la), (_, lb)| la.total_cmp(lb))
+        .unwrap()
+        .0
+}
+
+fn index_of_min(colors: &[Color]) -> usize {
+    colors
+        .iter()
+        .map(|c| c.luminance())
+        .enumerate()
+        .min_by(|(_, la), (_, lb)| la.total_cmp(lb))
+        .unwrap()
+        .0
+}
+
+fn segment_theme(rng: &mut dyn rand::RngCore, theme: &[Color]) -> (Color, Color, Vec<Color>) {
+    let min = index_of_min(theme);
+    let max = index_of_max(theme);
+    let rest = theme
+        .iter()
+        .enumerate()
+        .filter(|&(i, _)| i != max && i != min)
+        .map(|(_, c)| *c)
+        .collect::<Vec<_>>();
+    if rng.gen::<bool>() {
+        (theme[min], theme[max], rest)
+    } else {
+        (theme[max], theme[min], rest)
+    }
+}
+
 fn select_nodes(
     rng: &mut dyn rand::RngCore,
     grid: &Grid,
@@ -102,21 +138,21 @@ pub fn render(opts: &dyn RenderOpts, ctx: &Context, args: &Args) -> Result<(), B
     let themes = opts.themes()?;
     let (_, theme) = themes.pick(&mut rng);
 
+    let (ca, cb, colors) = segment_theme(&mut rng, &theme);
+
     ctx.save()?;
     ctx.new_path();
     ctx.rectangle(0.0, 0.0, width, height);
-    theme[0].set(ctx);
+    ca.set(ctx);
     ctx.fill()?;
     ctx.restore()?;
-
-    let c = color_contrasting_with(&theme[0]);
 
     let grid = Grid::new(width, height, rng.gen_range(20..50), rng.gen_range(5..20));
 
     if args.show_grid {
         ctx.save()?;
         grid.render(ctx)?;
-        c.set(ctx);
+        cb.set(ctx);
         ctx.set_dash(&[1.0, 4.0], 0.0);
         ctx.set_line_width(1.0);
         ctx.stroke()?;
@@ -132,12 +168,12 @@ pub fn render(opts: &dyn RenderOpts, ctx: &Context, args: &Args) -> Result<(), B
     }
     ctx.set_line_width(4.0);
     ctx.set_line_cap(LineCap::Round);
-    c.set(ctx);
+    cb.set(ctx);
     ctx.stroke()?;
     ctx.restore()?;
 
     let r = grid.dx.min(grid.dy) / 3.0;
-    let nodes = select_nodes(&mut rng, &grid, &theme[1..]);
+    let nodes = select_nodes(&mut rng, &grid, &colors);
     ctx.save()?;
     ctx.set_line_width(4.0);
     for (i, nodes) in nodes.iter().enumerate() {
@@ -146,7 +182,7 @@ pub fn render(opts: &dyn RenderOpts, ctx: &Context, args: &Args) -> Result<(), B
             ctx.arc(grid.x_of(i), grid.y_of(*j), r, 0.0, TAU);
             color.set(ctx);
             ctx.fill_preserve()?;
-            c.set(ctx);
+            cb.set(ctx);
             ctx.stroke()?;
         }
     }
